5326204b1cb0e7d951edff79eaa683c81f3361af
Arseny Konovalov <k.arseny03@gmail.com>
2024-04-03 15:28:23 +0300

    added log (poleno)

--- src/main/java/org/senechka/log/LOG.java
+++ src/main/java/org/senechka/log/LOG.java
@@ -1,4 +1,33 @@
 package org.senechka.log;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.IOException;
+import java.io.Writer;
+
 public class LOG {
+    private final LN ln;
+
+    public LOG(LN ln) {
+        this.ln = ln;
+    }
+
+    public LOG() {
+        this.ln = new LN();
+    }
+
+    public double log(double a, double b, double epsilon) {
+        return ln.calculate(b, epsilon) / ln.calculate(a, epsilon);
+    }
+
+    public double writeResultToCSV(double a, double x, double epsilon, Writer out) {
+        double res = log(a, x, epsilon);
+        try (CSVPrinter printer = CSVFormat.DEFAULT.print(out)) {
+            printer.printRecord(x, res);
+        } catch (IOException e) {
+            System.out.println("Corrupted filename");
+        }
+        return res;
+    }
 }
